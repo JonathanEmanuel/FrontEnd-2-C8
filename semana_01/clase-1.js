c2c68d801025a270163c19cde0ba14d9b518fce3
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-10-17 20:16:46 -0300

    Clase 01 - Piedra Papel o tijera

---
+++ semana_01/clase-1.js
@@ -0,0 +1,23 @@
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 1                                 */
+/* -------------------------------------------------------------------------- */
+function iniciarJuego(){
+    // Saludo y solicito el nombre
+    alert('Bienvendio Al Juego de Piedra papel o tijera');
+    let nombre = prompt('Ingrese su nombre');
+
+    alert(' Hola ' + nombre + '!'  );
+
+    return nombre;
+}
+
+// Creamos una variable Global con el nombre del jugador
+const nombreJugador = iniciarJuego();
+
+/* -------------------------------------------------------------------------- */
+/*                       CONSIGNA DE LA MESA DE TRABAJO                       */
+/* -------------------------------------------------------------------------- */
+
+// 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
+// 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
+// 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
