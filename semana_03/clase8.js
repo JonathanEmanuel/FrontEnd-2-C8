1f3877a90e3a6a01822bfb1939fdcd9b8af76d9f
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-11-01 19:24:34 -0300

    Clase 08 - Eventos

---
+++ semana_03/clase8.js
@@ -0,0 +1,35 @@
+/* -------------------------------------------------------------------------- */
+/*                        [4] FUNCION: Marcar favorito                        */
+/* -------------------------------------------------------------------------- */
+// - Buscar el album por id en el array 
+// - Cambiar el estado del like
+// - Volver a renderizar
+function marcarFavorito() {
+   
+
+}
+marcarFavorito()
+
+
+
+/* ----------------------------- MESA DE TRABAJO ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                         [5] FUNCION: Eliminar album                        */
+/* -------------------------------------------------------------------------- */
+// Debemos desarrollar la funcion de eliminar un album. Para esto le vamos a 
+// preguntar al usuario cual quiere eliminar.
+// Vamos a seguir las indicaciones que nos permiten lograrlo utilizando eventos.
+// 1- Hay que escuchar el evento de 'keydown' para detectar cuando el usuario
+// presiona la tecla f
+// 2- Una vez que detectamos la tecla, debemos mostrarle un prompt al usuario
+// para que ingrese el nombre del album que desea eliminar
+// 3- Podemos buscar la posicion del almbum buscado en el array con la funcion .findIndex()
+// 4- Si la busqueda nos da un resultado válido, procedemos a borrar el objeto del array
+// 5- Acto seguido debemos llamar a las funciones de renderizar y marcar favorito para que sean nuevamente aplicadas.
+
+function eliminarAlbum() {
+    // desarrollar la función 👇
+
+
+}
+eliminarAlbum();
