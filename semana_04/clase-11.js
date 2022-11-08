e8d0d071296237613caadb3bfd42c5d9b4d9aecb
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-11-08 19:30:32 -0300

    Clase 11 - Formularios

---
+++ semana_04/clase-11.js
@@ -0,0 +1,34 @@
+/* -------------------------------------------------------------------------- */
+/*               [1] FUNCION: capturar los datos del formulario               */
+/* -------------------------------------------------------------------------- */
+function capturarDatosFormulario() {
+
+};
+
+/* -------------------------------------------------------------------------- */
+/*                 [2] FUNCION: escuchamos el submit del form                 */
+/* -------------------------------------------------------------------------- */
+
+
+
+
+
+/* ----------------------------- MESA DE TRABAJO ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                         [3] FUNCION: validar datos                         */
+/* -------------------------------------------------------------------------- */
+// Desarrollar la funcion 3 de validar los datos.
+// Esta funcion va a recibir un objeto con la misma estructura de obejetoInformacion
+// Entonces dentro de esta función vamos a chequear ciertas validaciones.
+// 1- La funcion devuelve un listado de errores según las comprobaciones que hace sobre el objeto.
+// 2- Si el nombre no es un texto y tiene menos de 3 caracteres sumar el error: "El nombre debe tener al menos 3 caracteres."
+// 3- Si la contraseña tiene menos de 6 caracteres, sin contar espacios al principio, en el medio o final, sumar el error: "La contraseña debe tener al menos 6 caracteres, entre letras y símbolos."
+// 4- Si el telefono tiene menos de 10 números, sumar el error: "No es un teléfono válido."
+// 5- Si la lista de hobbies tiene más de 4 items, sumar el error: "Sólo es posible seleccionar 4 hobbies."
+// 5- Si no hay una nacionalidad definida, sumar el error: "Debe seleccionar una nacionalidad."
+function validarInformacion(usuario) {
+    let errores = [];
+    // 👇 desarrollar aqui la funcion
+
+    return errores;
+}
