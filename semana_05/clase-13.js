25ff330a4339035252fb935b974955bfff539e63
Jonathan Cruz <cruzjonathan.e@gmail.com>
2022-11-14 20:37:45 -0300

    Clase 13 - Validaciones

--- semana_05/clase-13.js
+++ semana_05/clase-13.js
@@ -1,7 +1,20 @@
+/* --------------------------- Estados por default -------------------------- */
+const estadoUsuario = {
+    email: '',
+    password: '',
+    rol: '',
+    terminos: false
+}
+
+const estadoErroresOK = {
+    email: false,
+    password: false,
+    rol: false,
+    terminos: false
+}
 
 
 /* ---------------------------------- nodos --------------------------------- */
-
 // Capturamos todos los elementos que necesitamos
 const formulario = document.querySelector('form');
 
@@ -20,19 +33,93 @@ const terminosError = document.querySelector('#terminosError');
 /*                   [1] FUNCION: mostrar errores al usuario                  */
 /* -------------------------------------------------------------------------- */
 function mostrarErrores() {
+    // Version con IFs
+    if( estadoErroresOK.email ){
+        emailError.classList.add('visible');
+    } else {
+        emailError.classList.remove('visible');
+    }
+
+    estadoErroresOK.password ? passwordError.classList.add('visible') : passwordError.classList.remove('visible');
+    estadoErroresOK.rol ? rolError.classList.add('visible') : rolError.classList.remove('visible');
+    (estadoErroresOK.terminos) ? terminosError.classList.add('visible') : terminosError.classList.remove('visible');
 
 }
 
 /* -------------------------------------------------------------------------- */
 /*               [2] FUNCION: actulizamos los estados de la app               */
 /* -------------------------------------------------------------------------- */
+formulario.addEventListener('change', function(){
+    console.log('Algo cambio')
+    // Normalizar los datos antes de agregarlos al objeto
 
+    estadoUsuario.email = inputEmail.value;
+    estadoUsuario.password = inputPassword.value;
+    estadoUsuario.rol = inputRol.value;
+    estadoUsuario.terminos = inputTerminos.checked; 
+
+    // Actualizo los estados
+    estadoErroresOK.email = validarEmail(estadoUsuario.email);
+    estadoErroresOK.password = validarPassword(estadoUsuario.password);
+    estadoErroresOK.rol = validarRol( estadoUsuario.rol);
+    estadoErroresOK.terminos = validarTerminos(estadoUsuario.terminos);
+
+    console.log(estadoUsuario);
+
+    mostrarErrores();
+})
 
 
 /* -------------------------------------------------------------------------- */
 /*                        [3] FUNCIONES: validar campos                       */
 /* -------------------------------------------------------------------------- */
+function validarEmail(email){
+    let resultado = false;
 
+    if( !email.includes('@') || !email.includes('.') ||  email.length < 5 ){
+        resultado = true;
+    }
+
+/*     let expReg = '/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;';
+
+    if(  expReg.test(  email )  ){
+        resultado = true;
+    }
+ */
+
+    return resultado;
+}
+
+function validarPassword(password){
+    let resultado = false;
+
+    if( password.length < 5 || password.includes(' ') ) {
+        resultado = true;
+    }
+
+    return resultado;
+}
+
+function validarRol(rol){
+    let resultado = false;
+
+    if( rol === '' ){
+        resultado = true;
+    }
+
+
+    return resultado;
+}
+
+function validarTerminos(verificacion){
+    let resultado = false;
+
+    if( !verificacion  ){
+        resultado = true;
+    }
+
+    return resultado;
+}
 
 /* -------------------------------------------------------------------------- */
 /*                      [4] FUNCION: escuchamos el submit                     */
@@ -40,7 +127,11 @@ function mostrarErrores() {
 
 // en el evento submit nos remitimos a chequear nuestro estado de errores
 formulario.addEventListener('submit', function (evento) {
+    evento.preventDefault();
 
+    // LO IMPLEMENTAMOS LA PROXIMA
+
+    console.log('Enviando datos')
 
 });
 
@@ -54,7 +145,7 @@ formulario.addEventListener('submit', function (evento) {
 // Esta funcion se va a encargar de realizar la redirección cuando el formulario se complete correctamente.
 // Para eso debera cumplir con los siguientes requerimientos.
 // 1 - Deshabilitar el boton del formulario.
-// 2 - Esperar 3 segundos para redireccionar a la página de 
+// 2 - Esperar 3 segundos para redireccionar a la página de Usuario
 // 3 - Durante ese tiempo el boton deshabilitado debe mostrar el texto: "Cargando..."
 // 4 - Cuando vaya a la página de 'usuario.html' NO se debe permitir que mediante el botón de "Atrás"(la flechita del navegador) el usuario vuelva a index.
 
